47d8f113c1183d8d138bfd388cda47aff1300c25
MLM MOVIE STORE <86915159+arjunsangu@users.noreply.github.com>
2021-07-16 18:31:30 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -52,8 +52,6 @@ async def start(bot, update):
         InlineKeyboardButton('🔰CHANNEL 1🔰', url ='https://t.me/mlm_movie_1')
         InlineKeyboardButton('🔰CHANNEL 2🔰', url ='https://t.me/mlm_movie_2'),
     ],[
-        InlineKeyboardButton('🔰UPDATE CHANNEL🔰', url ='https://t.me/mlm_movies_update'),
-    ],[
         InlineKeyboardButton('OWNER👨‍✈️', url='https://t.me/mrplantozz')
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
