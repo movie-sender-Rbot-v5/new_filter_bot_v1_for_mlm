dc6d5229b508570712c5c79fc1b3930e09516215
MLM MOVIE STORE <86915159+arjunsangu@users.noreply.github.com>
2021-07-17 19:39:22 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -45,11 +45,11 @@ class Translation(object):
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/mrplantozz">MY BOZZ</a></i></b>
+<b>➥ CREATOR</b> : <b><i> @mrplantozz_bot
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➥ LANGUAGE</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ LIBRARY</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ SOURCE CODE</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
