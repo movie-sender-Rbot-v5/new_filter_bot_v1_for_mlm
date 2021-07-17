ba3664b32a1d53f3ca6ddaf93b56a8b10bb0101e
MLM MOVIE STORE <86915159+arjunsangu@users.noreply.github.com>
2021-07-17 18:50:04 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"🔍FOUND {(len_results)} FILES🎥 \n  📥<code>{query}<code>📥",
+                text=f"🔍FOUND {(len_results)} FILES FOR YOUR REQEST ➧ <code>{query}<code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
