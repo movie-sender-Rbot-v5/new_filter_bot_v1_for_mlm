674c6388c3c526cd2100f1a370111576aa1bf705
MLM MOVIE STORE <86915159+arjunsangu@users.noreply.github.com>
2021-07-18 15:47:13 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -54,7 +54,7 @@ async def start(bot, update):
         InlineKeyboardButton('OWNER 👨‍✈️', url='https://t.me/mrplantozz_bot'),
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ],[
-        InlineKeyboardButton('POP UP TESTING', message='NOTHING'),
+        InlineKeyboardButton('POP UP TESTING', popup='NOTHING'),
     ],[
        InlineKeyboardButton('CLOSE 🔒', callback_data='close')
     ]]
