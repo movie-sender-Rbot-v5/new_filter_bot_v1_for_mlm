fac5d6fd128fbb503384243edacf569cbeccbc63
MLM MOVIE STORE <86915159+arjunsangu@users.noreply.github.com>
2021-07-18 16:33:10 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -155,7 +155,7 @@ async def auto_filter(bot, update):
         result[0].append([
             InlineKeyboardButton(f"🎗️ Page 1/{len_result if len_result < max_pages else max_pages} 🎗️", callback_data="ignore"),
          ],[
-            InlineKeyboardButton('HOW TO GET FILES 🤔', url='https://t.me/mlm_movies_update/18'),
+            InlineKeyboardButton("HOW TO GET FILES 🤔", url="https://t.me/mlm_movies_update/18"),
         ])
         
         
